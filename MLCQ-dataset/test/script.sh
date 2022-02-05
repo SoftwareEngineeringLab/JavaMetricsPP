5f848e92c468fd4f55042cbea8b5d0397aab5134
Przemyslaw Jambor ext <Przemyslaw.Jambor.ext@msg.group>
2022-02-05 16:07:20 +0100

    Adding script

---
+++ MLCQ-dataset/test/script.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# download files
+
+wget -O csv-conform-1.0-SNAPSHOT-jar-with-dependencies.jar https://raw.githubusercontent.com/SoftwareEngineeringLab/JavaMetricsPP/3c199b5fcb743a1a9c1cf572c82f6e3fa4c4aab4/MLCQ-dataset/test/csv-conform-1.0-SNAPSHOT-jar-with-dependencies.jar;
+
+wget -O metrics-calculator-1.0-SNAPSHOT-jar-with-dependencies.jar https://raw.githubusercontent.com/SoftwareEngineeringLab/JavaMetricsPP/3c199b5fcb743a1a9c1cf572c82f6e3fa4c4aab4/MLCQ-dataset/test/metrics-calculator-1.0-SNAPSHOT-jar-with-dependencies.jar;
+
+wget -O MLCQ_small.csv https://raw.githubusercontent.com/SoftwareEngineeringLab/JavaMetricsPP/3c199b5fcb743a1a9c1cf572c82f6e3fa4c4aab4/MLCQ-dataset/test/MLCQ_small.csv;
+
+# clone repositories
+{
+	read
+	while IFS= read -r line; do
+		IFS=';' read -ra ADDR <<< "$line"
+		repository_address="${ADDR[8]}"
+		revision="${ADDR[9]}"
+		link="${ADDR[13]}"
+		
+		IFS='/' read -ra TAB <<< "$link"
+		user_name="${TAB[3]}"
+		project_name="${TAB[4]}"
+		
+		mkdir "${user_name}"
+		cd "${user_name}"
+		
+		git clone "${repository_address}"
+		
+		cd "${project_name}"
+		git reset --hard "${revision}"
+		
+		cd ..
+		cd ..
+	done
+	
+} < MLCQ_small.csv;
+
+
+# calculate metrics
+java -jar csv-conform-1.0-SNAPSHOT-jar-with-dependencies.jar MLCQ_small.csv MLCQ_conformed.csv;
+
+java -jar metrics-calculator-1.0-SNAPSHOT-jar-with-dependencies.jar -csv $PWD/MLCQ_conformed.csv -gitsources $PWD;
